acc3ba53a8504a3927e292da3f0af2dff2d7892b
Ankit-7484 <155381388+Ankit-7484@users.noreply.github.com>
2024-01-01 23:54:48 +0530

    C Program to Add Two Numbers

---
+++ C Program to Add Two Numbers.c
@@ -0,0 +1,11 @@
+/*C Program to Add Two Numbers
+24 - December-2023
+Ankit Kumar Singh
+*/
+#include<stdio.h>
+main ()
+{ int a , b;
+ printf ("Enter Two Numbers : ");
+ scanf ("%d%d", & a , & b);
+ printf ("%d + %d = %d", a , b , a+b);
+}
