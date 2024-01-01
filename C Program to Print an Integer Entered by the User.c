cf4355a11ad649bfcc1de8584004d00ffb5993b4
Ankit-7484 <155381388+Ankit-7484@users.noreply.github.com>
2024-01-01 23:50:37 +0530

    C Program to Print an Integer Entered by the User

---
+++ C Program to Print an Integer Entered by the User.c
@@ -0,0 +1,13 @@
+/*C Program to Print an Integer Entered by the User
+
+24 - December-2023
+
+Ankit Kumar Singh
+*/
+#include<stdio.h>
+main ()
+{ int i;
+ printf ("Enter The Number : ");
+ scanf ("%d", & i);
+ printf ("You Have Entered %d", i);
+}
