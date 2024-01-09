e4d7c15352b14a1f3828ef0bb0fa88fabf8444cc
Ankit-7484 <155381388+Ankit-7484@users.noreply.github.com>
2024-01-09 18:07:03 +0530

    C Program to Multiply two Floating-Point Numbers

---
+++ C Program to Multiply two Floating-Point Numbers.c
@@ -0,0 +1,12 @@
+/* C Program to Multiply two Floating-Point Numbers
+25-December-2023
+Ankit Kumar Singh
+*/
+#include<stdio.h>
+main()
+{
+         float a , b ;
+         printf("Enter Two numbers : ");
+         scanf("%f%f",&a,&b);
+         printf("%f X %f = %f",a , b , a*b);
+}
